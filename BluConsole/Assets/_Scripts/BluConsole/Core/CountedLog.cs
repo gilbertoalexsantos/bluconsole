6040a4fdf4a9cb056f2ea5ad4e0a240931eb3532
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2016-11-05 16:26:04 -0300

    Implemented CountedLog

---
+++ BluConsole/Assets/_Scripts/BluConsole/Core/CountedLog.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BluConsole
+{
+
+public class CountedLog
+{
+
+    private LogInfo _log;
+    private int _quantity;
+
+    public LogInfo Log
+    {
+        get
+        {
+            return _log;
+        }
+    }
+
+    public int Quantity
+    {
+        get
+        {
+            return _quantity;
+        }
+        set
+        {
+            _quantity = value;
+        }
+    }
+
+    public CountedLog(
+        LogInfo log,
+        int quantity)
+    {
+        _log = log;
+        _quantity = quantity;
+    }
+	
+}
+
+}
