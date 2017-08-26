6e2a955341288fbf0568367b2a498dc24b08f59d
Gilberto Alexandre dos Santos <gilberto.alexsantos@gmail.com>
2017-08-26 20:59:45 -0300

    Fixed bug that was not opening file on correct IDE on windows

--- BluConsole/Assets/BluConsole/Scripts/Editor/BluUtils.cs
+++ BluConsole/Assets/BluConsole/Scripts/Editor/BluUtils.cs
@@ -6,54 +6,57 @@ using System.Reflection;
 namespace BluConsole.Editor
 {
 
-public static class BluUtils
-{
-
-    static List<string> defaultIgnorePrefixes = new List<string>()
+    public static class BluUtils
     {
-        "UnityEngine.Debug"
-    };
 
-    public static GUIContent GUIContent(this string text)
-    {
-        return new GUIContent(text);
-    }
-
-    public static void OpenFileOnEditor(string path, int line)
-    {
-        var filename = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), path);
-        if (System.IO.File.Exists(filename))
-            UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(path, line);
-    }
-
-    public static List<string> StackTraceIgnorePrefixs
-    {
-        get
+        static List<string> defaultIgnorePrefixes = new List<string>()
         {
-            var ret = new List<string>(defaultIgnorePrefixes);
-            var stackTraceIgnoreType = typeof(StackTraceIgnore);
+            "UnityEngine.Debug"
+        };
 
-            var assembly = Assembly.GetAssembly(stackTraceIgnoreType);
-            foreach (var type in assembly.GetTypes())
+        public static GUIContent GUIContent(this string text)
+        {
+            return new GUIContent(text);
+        }
+
+        public static void OpenFileOnEditor(string path, int line)
+        {
+            var asset = AssetDatabase.LoadAssetAtPath(path, typeof(MonoScript));
+            if (asset == null)
+                return;
+
+            AssetDatabase.OpenAsset(asset.GetInstanceID(), line);
+            Resources.UnloadAsset(asset);
+        }
+
+        public static List<string> StackTraceIgnorePrefixs
+        {
+            get
             {
-                foreach (var method in type.GetMethods(BindingFlags.Public | 
-                                                    BindingFlags.NonPublic |
-                                                    BindingFlags.Static |
-                                                    BindingFlags.Instance |
-                                                    BindingFlags.Default))
+                var ret = new List<string>(defaultIgnorePrefixes);
+                var stackTraceIgnoreType = typeof(StackTraceIgnore);
+
+                var assembly = Assembly.GetAssembly(stackTraceIgnoreType);
+                foreach (var type in assembly.GetTypes())
                 {
-                    if (method.GetCustomAttributes(stackTraceIgnoreType, true).Length > 0)
+                    foreach (var method in type.GetMethods(BindingFlags.Public | 
+                                                           BindingFlags.NonPublic |
+                                                           BindingFlags.Static |
+                                                           BindingFlags.Instance |
+                                                           BindingFlags.Default))
                     {
-                        var key = string.Format("{0}:{1}", method.DeclaringType.FullName, method.Name);
-                        ret.Add(key);
+                        if (method.GetCustomAttributes(stackTraceIgnoreType, true).Length > 0)
+                        {
+                            var key = string.Format("{0}:{1}", method.DeclaringType.FullName, method.Name);
+                            ret.Add(key);
+                        }
                     }
                 }
-            }
 
-            return ret;
+                return ret;
+            }
         }
-    }
 	
-}
+    }
 
 }
